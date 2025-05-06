3c887ba01934001abd2e8a54c7d74a7607f3c9d7
arthurmoltke@gmail.com <arthurmoltke@gmail.com>
2025-05-06 15:54:28 +0000

    int orders

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,4 +1,6 @@
-select * 
-from  {{ ref('stg_raw__sales')}} 
-left join {{ ref('stg_raw__product')}} 
+select *,
+b.purchase_price * a.revenue as purchase_cost,
+a.revenue - (a.quantity * b.purchase_price) as margin,
+from  {{ ref('stg_raw__sales')}} as a 
+left join {{ ref('stg_raw__product')}} as b
 using(products_id)
